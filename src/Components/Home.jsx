c5c06163f6fad5e9c544b9b452c91834da45a710
Ethan <92686703+not-a-ethan@users.noreply.github.com>
2023-08-30 04:41:32 +0000

    added something

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -35,6 +35,7 @@ const Home = ({ name, title }) => {
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
         <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
       </div>
+      <p>Hi</p>
     </section>
   );
 };
